2e6621bfee39f0b02626e6658a49b9fc7f40de66
Ismail Mohamad <ikmohamad19@gmail.com>
2022-12-06 20:30:11 +0000

    feat: added datepicker, and created utils for date

--- screens/authenticatedScreens/rides/CreateARideScreen.js
+++ screens/authenticatedScreens/rides/CreateARideScreen.js
@@ -2,10 +2,14 @@ import { useState } from "react"
 import { View, StyleSheet, ScrollView } from "react-native"
 import { Button, Text, TextInput } from "react-native-paper"
 import { RadioInput } from "../../../components/RadioInput"
+import DateTimePicker from "@react-native-community/datetimepicker"
+import { formatTime, formatDate } from "../../../utils/formatDate"
 
 export const CreateARideScreen = () => {
+  const [mode, setMode] = useState("date")
+  const [show, setShow] = useState(true)
   const [rideData, setRideData] = useState({
-    date: "",
+    date: new Date(),
     name: "",
     rideType: "",
     startLocation: [],
@@ -14,6 +18,23 @@ export const CreateARideScreen = () => {
     description: "",
     cafeStops: "",
   })
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate
+    setShow(false)
+    setRideData({ ...rideData, date: currentDate })
+  }
+
+  const showDatepicker = () => {
+    setMode("date")
+    setShow(true)
+  }
+
+  const showTimepicker = () => {
+    setMode("time")
+    setShow(true)
+  }
+
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -28,13 +49,32 @@ export const CreateARideScreen = () => {
               onChangeText={(text) => setRideData({ ...rideData, name: text })}
             />
           </View>
-          <View style={styles.formInputs}>
-            <TextInput
-              label="Date"
-              value={rideData.date}
-              onChangeText={(text) => setRideData({ ...rideData, date: text })}
-            />
+          <View style={[styles.formInputs, styles.dateContainer]}>
+            <Button
+              mode="contained"
+              style={styles.dateButton}
+              onPress={showDatepicker}
+              icon="calendar-outline"
+            >
+              {formatDate(rideData.date)}
+            </Button>
+            <Button
+              mode="contained"
+              style={styles.dateButton}
+              onPress={showTimepicker}
+              icon="calendar-clock-outline"
+            >
+              {formatTime(rideData.date)}
+            </Button>
           </View>
+          {show && (
+            <DateTimePicker
+              value={rideData.date}
+              mode={mode}
+              is24Hour={true}
+              onChange={onChange}
+            />
+          )}
           <View style={styles.formInputs}>
             <RadioInput
               radioData={["Training", "Casual"]}
@@ -113,6 +153,14 @@ const styles = StyleSheet.create({
   },
   formInputs: {
     marginBottom: 10,
+  },
+  dateContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  dateButton: {
+    width: "45%",
   },
   button: {
     marginTop: 10,
