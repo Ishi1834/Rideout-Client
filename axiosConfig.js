463e4f9ce9f1da1b9db416d04b21203816abc6f5
Ismail Mohamad <ikmohamad19@gmail.com>
2022-12-24 22:50:04 +0000

    chore: added interceptor to refresh auth tokenon 403

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,5 +1,20 @@
-import axios from "axios"
+import axiosBase from "axios"
+import * as SecureStore from "expo-secure-store"
 
-export default axios.create({
+const axios = axiosBase.create({
   baseURL: "http://10.0.2.2:3500/",
 })
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 403) {
+      const refreshToken = await SecureStore.getItemAsync("refreshToken")
+      const res = await axios.post("auth/refresh", { refreshToken })
+      const { authToken } = res.data
+      axios.defaults.headers.common["Authorization"] = `Bearer ${authToken}`
+    }
+    return error
+  }
+)
+export default axios
