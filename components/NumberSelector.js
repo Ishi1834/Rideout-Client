36475c8d292034e7283a36a55c8d7afab5b8bc98
Ismail Mohamad <ikmohamad19@gmail.com>
2023-01-04 21:39:40 +0000

    chore: add component to handle numbers

---
+++ components/NumberSelector.js
@@ -0,0 +1,88 @@
+import { useState } from "react"
+// UI
+import { StyleSheet, View } from "react-native"
+import { TextInput, Button, Text } from "react-native-paper"
+// Other
+import * as yup from "yup"
+
+export const NumberSelector = ({
+  initialNumber = 0,
+  label = "",
+  handleNumberChange,
+}) => {
+  const [selectedNumber, setSelectedNumber] = useState(initialNumber)
+
+  const numberSchema = yup.object().shape({
+    number: yup.number().required(),
+  })
+
+  const checkNumberIsValid = async (number) => {
+    const isValid = await numberSchema.isValid({ number })
+
+    return isValid
+  }
+
+  const handleMinus = async () => {
+    const newNum = selectedNumber - 1
+    if (await checkNumberIsValid(newNum)) {
+      setSelectedNumber(newNum)
+      handleNumberChange(newNum)
+    }
+  }
+
+  const handleAdd = async () => {
+    const newNum = selectedNumber + 1
+    if (await checkNumberIsValid(newNum)) {
+      setSelectedNumber(newNum)
+      handleNumberChange(newNum)
+    }
+  }
+
+  const handleChange = async (number) => {
+    const newNum = parseInt(number)
+    if (await checkNumberIsValid(newNum)) {
+      setSelectedNumber(newNum)
+      handleNumberChange(newNum)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.numberContainer}>
+        <Button icon="minus" style={styles.button} onPress={handleMinus} />
+
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          value={`${selectedNumber}`}
+          mode="outlined"
+          onChangeText={(number) => handleChange(number)}
+        />
+
+        <Button icon="plus" style={styles.button} onPress={handleAdd} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  label: {
+    textAlign: "center",
+  },
+  numberContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: 40,
+  },
+  input: {
+    textAlign: "center",
+  },
+})
