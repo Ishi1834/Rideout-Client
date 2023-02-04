2b623d2b05e98857a1e8ea6b91e202e5817bd2f3
Ismail Mohamad <ikmohamad19@gmail.com>
2023-02-04 15:16:30 +0000

    chore: create email not verified modal

---
+++ components/EmailNotVerifiedModal.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { StyleSheet } from "react-native"
+import { Button, Portal, Modal, Text, Card } from "react-native-paper"
+import { emailNotVerifiedContent } from "../static/modalContent"
+
+export const EmailNotVerifiedModal = ({ handleDismiss, emailVerified }) => {
+  const [showModal, setShowModal] = useState(!emailVerified ? true : false)
+  return (
+    <Portal>
+      <Modal
+        visible={showModal}
+        contentContainerStyle={styles.modalStyle}
+        dismissable={false}
+      >
+        <Text style={styles.modalText}>{emailNotVerifiedContent}</Text>
+        <Card.Actions>
+          <Button
+            onPress={() => {
+              setShowModal(false)
+              handleDismiss()
+            }}
+          >
+            Ok
+          </Button>
+        </Card.Actions>
+      </Modal>
+    </Portal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalStyle: { backgroundColor: "white", padding: 20, marginHorizontal: 20 },
+  modalText: {
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    fontSize: 15,
+  },
+})
---
+++ static/modalContent.js
@@ -0,0 +1,2 @@
+export const emailNotVerifiedContent =
+  "Verify Your Email for Full Access \n\nIt looks like you're trying to use a feature that requires email verification. No worries, it's a quick process! \n\nTo verify your email, simply go to the profile page in the app and follow the instructions. Once your email is verified, you'll have full access to all features. \n\nIf you have any trouble, don't hesitate to contact our support team for assistance. \n\nThank you for using our app!"
